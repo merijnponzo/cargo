d19bb44b9fd367c22476d1b4a6d582e3348e2494
Merijn <merijn@ponzo.nl>
2023-09-07 15:27:13 +0200

    toggle

--- web/app/themes/blockbite-theme/blocks/custom-nav/menu.php
+++ web/app/themes/blockbite-theme/blocks/custom-nav/menu.php
@@ -35,7 +35,7 @@ foreach ($menuNested as $key => $item) {
         <?php foreach ($menuNested as $key => $item) : ?>
             <li>
                 <?php if (isset($item->submenu)) : ?>
-                    <a href="javascript:void(0)" @click="open = <?php echo $item->ID; ?>" class="<?php echo $item->classes; ?>">
+                    <a href="javascript:void(0)" @click="!open ? open = <?php echo $item->ID; ?> : open = false" class="<?php echo $item->classes; ?>">
                         <svg xmlns="http://www.w3.org/2000/svg" class="angle-down" viewBox="0 0 24 24" x-bind:class="(open === <?php echo $item->ID; ?>) ? 'open' : 'closed'">
                             <path d="M17,9.17a1,1,0,0,0-1.41,0L12,12.71,8.46,9.17a1,1,0,0,0-1.41,0,1,1,0,0,0,0,1.42l4.24,4.24a1,1,0,0,0,1.42,0L17,10.59A1,1,0,0,0,17,9.17Z" />
                         </svg>
